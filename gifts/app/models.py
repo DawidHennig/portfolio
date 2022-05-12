536d2f61b52fbeba1d96a6944658343cd2265001
Dawid <zgredzik987@gmail.com>
2022-05-12 14:27:44 +0200

    models fix

--- gifts/app/models.py
+++ gifts/app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Category(models.Model):
-    name = models.CharField(max_lenght=30)
+    name = models.CharField(max_length=30)
 
 
 class Institution(models.Model):
@@ -14,8 +14,8 @@ class Institution(models.Model):
         (2, "organizacja pozarządowa"),
         (3, "zbiórka lokalna"),
         )
-    name = models.CharField(max_lenght=30)
-    description = models.CharField(max_lenght=300)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
     i_type = models.IntegerField(choices=TYPES, default=1) 
     categories = models.ManyToManyField(Category)
 
@@ -24,10 +24,10 @@ class Donation(models.Model):
     quantity = models.IntegerField()
     categories = models.ManyToManyField(Category)
     institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
-    address = models.CharField(max_lenght=30)
-    phone_number = models.CharField(max_lenght=30)
-    city = models.CharField(max_lenght=30)
-    zip_code = models.CharField(max_lenght=6)
+    address = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    zip_code = models.CharField(max_length=6)
     pick_up_date = models.DateField()
     pick_up_time = models.DateTimeField()
     pick_up_comment = models.TextField()
